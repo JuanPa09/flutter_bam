bfe28429643260d144facb35da187beaa571f7d7
Jorge Ordonez <jorge.ordonez@bam.com.gt>
2026-06-30 11:24:46 -0600

    quit innecesaries files

--- bam_wallet/lib/features/home/data/data_sources/home_data_source.dart
+++
@@ -1,7 +0,0 @@
-import 'package:bam_wallet/features/home/data/models/bank_account.dart';
-import 'package:bam_wallet/features/home/data/models/transfer.dart';
-
-abstract class HomeDataSource {
-  Future<List<BankAccount>> getAccounts();
-  Future<List<Transfer>> getTransfers();
-}
--- bam_wallet/lib/features/home/domain/repositories/bank_account_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:bam_wallet/features/home/data/models/bank_account.dart';
-
-abstract class BankAccountRepository {
-  Future<List<BankAccount>> fetchBankAccounts();
-}
